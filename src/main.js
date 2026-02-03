7a5b9d78162e642ecf87578922c9af75d581144d
Moises <perezzaragoza.josemoises@gmail.com>
2026-02-02 21:45:41 -0600

    Configuracion Inicial de Archivos JS

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
-import './style.css'
+import './style.css';
+import {App} from './todos/app' ;
 
-console.log('Hola Mundo')
+
+App('#app');
---
+++ src/todos/app.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @param {String} elementId 
+ */
+
+
+export const App = (elementId)=>{
+
+
+    (()=>{
+
+
+        const app = document.createElement('div');
+        app.innerHTML = '<h1>Hola Mundo</h1>';
+        document.querySelector( elementId).append(app);
+
+})();
+};
