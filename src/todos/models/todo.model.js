f71216003370ab8ef9504366e1129411fda49150
Moises <perezzaragoza.josemoises@gmail.com>
2026-02-10 13:34:21 -0600

    Implementacion de uuid en todo.model.js

--- src/todos/models/todo.model.js
+++ src/todos/models/todo.model.js
@@ -1,8 +1,10 @@
+import {v4 as uuid} from 'uuid';
+
 export class Todo {
 
     constructor(description){
 
-        this.id = 1;
+        this.id = uuid();
         this.description = description;
         this.done = false;
         this.createdAt = new Date();
