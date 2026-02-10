7b0182708319ddc3b5a4b0943756193933063b87
Moises <perezzaragoza.josemoises@gmail.com>
2026-02-10 13:27:28 -0600

    Implementacion de store Aguacate

--- src/todos/app.js
+++ src/todos/app.js
@@ -10,7 +10,6 @@ export const App = (elementId)=>{
 
     (()=>{
 
-
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector( elementId).append(app);
