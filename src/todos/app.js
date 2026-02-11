40b02b7ef4ef9240420dc3cb6baca8f5cf32c7a3
Moises <perezzaragoza.josemoises@gmail.com>
2026-02-10 21:27:58 -0600

    Implementacion de render de todos

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,18 +1,33 @@
 import html from './app.html?raw';
+import todoStore from "../store/todo.store";
+import { renderTodos } from "./use-cases";
 
-/**
- * 
- * @param {String} elementId 
- */
+const ElementIDs = {
+    TodoList: '.todo-list',
+}
 
 
 export const App = (elementId)=>{
 
-    (()=>{
+    const displayTodos = ()=>{
+        const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+        renderTodos( ElementIDs.TodoList, todos);
 
+
+    }
+
+
+
+
+
+
+
+
+    // Cuando la App() se llama
+    (()=>{
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector( elementId).append(app);
-
+        displayTodos();
 })();
-};
+}
